08ea22843d9eac92601a732b6fea1f94f052d834
esipov03 <esipov.00_00@mail.ru>
2019-11-29 23:15:15 +0300

    test

---
+++ src/main/java/com/hakaton/budshet/BudshetApplication.java
@@ -0,0 +1,13 @@
+package com.hakaton.budshet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudshetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudshetApplication.class, args);
+	}
+
+}
